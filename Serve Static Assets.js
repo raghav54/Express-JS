d8959d9e2c8a1871302971addc8575d91d26b565
Raghav Singal <32788339+raghav54@users.noreply.github.com>
2021-10-26 22:29:09 +0530

    Update Serve Static Assets.js

--- Serve Static Assets.js
+++ Serve Static Assets.js
@@ -6,7 +6,7 @@ app.get('/',function(req, res) {
   res.send('Hello Express');
 })
 
-/app.get('/',function(req, res) {
+app.get('/',function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
