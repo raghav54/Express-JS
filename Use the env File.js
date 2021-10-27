433cd721d0ac416f0bca41446a14254be268cfb6
Raghav Singal <32788339+raghav54@users.noreply.github.com>
2021-10-27 11:01:21 +0530

    Update Use the env File.js

--- Use the env File.js
+++ Use the env File.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
-
+//MESSAGE_STYLE=uppercase - environment variable declaration
 app.get('/json',function(req, res) {
   if(process.env.MESSAGE_STYLE == 'uppercase'){
     res.json({"message": "HELLO JSON"});
